b32fc37ae47a031e4eafeaa6ae2fe31200e87af8
Astroboyag <46861452+Astroboyag@users.noreply.github.com>
2023-05-10 23:46:55 -0400

    Update Goal_7.sql

--- Goal_7.sql
+++ Goal_7.sql
@@ -1,27 +1,7 @@
- buffer_areas AS (
-  SELECT ST_Union(ST_Buffer(ST_Transform(geom, 3857), 500)) AS geom
-  FROM public.geo_features
-  WHERE amenity = 'cafe' AND building = 'yes'
-)
-SELECT ST_Area(ST_Transform(geom, 3857)) AS area_sq_meters
-FROM buffer_areas;
-WITH
-
-CREATE INDEX idx_cuisine ON public.geo_features (cuisine);
-
-
 SELECT gid, name, ST_AsText(geom) AS location
 FROM public.geo_features
 WHERE cuisine = 'coffee_shop'
 LIMIT 10;
- buffer_areas AS (
-  SELECT ST_Union(ST_Buffer(ST_Transform(geom, 3857), 500)) AS geom
-  FROM public.geo_features
-  WHERE amenity = 'cafe' AND building = 'yes'
-)
-SELECT ST_Area(ST_Transform(geom, 3857)) AS area_sq_meters
-FROM buffer_areas;
-WITH
 
 CREATE INDEX idx_geo_features_geom ON public.geo_features USING gist(geom);
 
@@ -31,24 +11,8 @@ SELECT gid, name, ST_AsText(geom) AS location,
 FROM public.geo_features
 WHERE amenity = 'cafe' AND ST_DWithin(ST_Transform(geom, 3857), ST_Transform(ST_SetSRID(ST_Point(-74.0014, 40.7289), 4326), 3857), 5000)
 ORDER BY distance_meters ASC
-LIMIT 5;
- buffer_areas AS (
-  SELECT ST_Union(ST_Buffer(ST_Transform(geom, 3857), 500)) AS geom
-  FROM public.geo_features
-  WHERE amenity = 'cafe' AND building = 'yes'
-)
-SELECT ST_Area(ST_Transform(geom, 3857)) AS area_sq_meters
-FROM buffer_areas;
-WITH
+LIMIT 5;                     
 
 CREATE INDEX idx_geo_features_geom ON public.geo_features USING gist(geom);
 
 CREATE INDEX idx_cuisine ON public.geo_features (cuisine);
- buffer_areas AS (
-  SELECT ST_Union(ST_Buffer(ST_Transform(geom, 3857), 500)) AS geom
-  FROM public.geo_features
-  WHERE amenity = 'cafe' AND building = 'yes'
-)
-SELECT ST_Area(ST_Transform(geom, 3857)) AS area_sq_meters
-FROM buffer_areas;
-WITH
